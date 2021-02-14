e11be2c9fdb970f5817d3a4dd84f1c0bd83a897d
IntelBear <bryantshao@gmail.com>
2021-02-13 20:08:44 -0800

    Finished Lab 6

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,20 @@
 // Script.js
 
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+  if(!localStorage.getItem("productData")){
+    fetch("https://fakestoreapi.com/products")
+    .then(response => response.json()).then(data => localStorage.setItem("productData",JSON.stringify(data)));
+  }
+
+  let products = JSON.parse(localStorage.getItem("productData"));
+  let prodContainer = document.getElementById("product-list");
+  for(let i = 0; i < products.length; i++){
+    let temp = new ProductItem(products[i]);
+
+    prodContainer.appendChild(temp);
+  }
+
+  if(localStorage.getItem('cart-count') !== null){
+    document.getElementById('cart-count').textContent = localStorage.getItem('cart-count');
+  }
 });
